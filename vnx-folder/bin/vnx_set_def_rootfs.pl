e3dfbfef56afb1d82fe002c2542132ef1b0ec88f
david-fdez <david-fdez@localhost>
2011-07-31 03:14:51 +0200

    Cambios en ejemplos

--- vnx-folder/bin/vnx_set_def_rootfs.pl
+++ vnx-folder/bin/vnx_set_def_rootfs.pl
@@ -1,30 +1,32 @@
 #!/usr/bin/perl
 
+use strict;
+
 my $usage = <<EOF;
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Virtual Networks over LinuX (VNX) -- http://www.dit.upm.es/vnx - vnx@dit.upm.es          
+Virtual Networks over LinuX (VNX) -- http://www.dit.upm.es/vnx - vnx\@dit.upm.es          
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 vnx_set_def_rootfs:  Creates links for the default VNX root filesystem
 
-Usage: vnx_set_def_rootfs <rootfs_name> <dist_version> <rootfs_version>
+Usage: vnx_set_def_rootfs <virtplatform> <rootfs_name> <dist_version> <rootfs_version>
 
 Examples: 
-  'vnx_set_def_rootfs ubuntu 10.04 v021' command creates the following link:
+  'vnx_set_def_rootfs kvm ubuntu 10.04 v021' command creates the following link:
 
         /usr/share/vnx/filesystems/rootfs_ubuntu -->
-            /usr/share/vnx/filesystems/vnx_rootfs_ubuntu-10.04-v021.qcow2
+            /usr/share/vnx/filesystems/vnx_rootfs_kvm_ubuntu-10.04-v021.qcow2
        
-  'vnx_set_def_rootfs fedora-gui 14 v021' command creates the following link:
+  'vnx_set_def_rootfs kvm fedora-gui 14 v021' command creates the following link:
 
         /usr/share/vnx/filesystems/vnx_rootfs_fedora-gui -->
-            /usr/share/vnx/filesystems/vnx_rootfs_fedora-14-gui-v021.qcow2
+            /usr/share/vnx/filesystems/vnx_rootfs_kvm_fedora-14-gui-v021.qcow2
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 EOF
 
 
 # Command line arguments process 
-if ($#ARGV != 2) {
+if ($#ARGV != 3) {
 	print "$usage";
     exit (1);
 }
@@ -37,15 +39,16 @@ for (my $i=0; $i <= $#ARGV; $i++) {
 }
 
 my $VNXFSDIR = "/usr/share/vnx/filesystems";
-my $rootfsType = $ARGV[0];
-my $distVersion = $ARGV[1];
-my $rootfsVersion = $ARGV[2];
+my $virtPlatform = $ARGV[0];
+my $rootfsType = $ARGV[1];
+my $distVersion = $ARGV[2];
+my $rootfsVersion = $ARGV[3];
 my $guiDist;
 my $rootfsFname;
 my $linkName;
 
 print "vnx_set_def_rootfs:   \n";
-print "    rootfs type=$rootfsType\n    distribution version=$distVersion\n    rootfs version=$rootfsVersion\n";
+print "    virt platform=$virtPlatform\n    rootfs type=$rootfsType\n    distribution version=$distVersion\n    rootfs version=$rootfsVersion\n";
 
 if ($rootfsType =~ /.*-gui/) {
 	$rootfsType =~ s/-gui//;
@@ -54,10 +57,10 @@ if ($rootfsType =~ /.*-gui/) {
 }
 
 if ($guiDist) {
-	$rootfsFname = "$VNXFSDIR/vnx_rootfs_$rootfsType-$distVersion-gui-$rootfsVersion.qcow2";
+	$rootfsFname = "$VNXFSDIR/vnx_rootfs_${virtPlatform}_${rootfsType}-${distVersion}-gui-${rootfsVersion}.qcow2";
 	$linkName = "$VNXFSDIR/rootfs_$rootfsType-gui";
 } else {
-	$rootfsFname = "$VNXFSDIR/vnx_rootfs_$rootfsType-$distVersion-$rootfsVersion.qcow2";
+	$rootfsFname = "$VNXFSDIR/vnx_rootfs_${virtPlatform}_${rootfsType}-${distVersion}-${rootfsVersion}.qcow2";
 	$linkName = "$VNXFSDIR/rootfs_$rootfsType";
 }
 
