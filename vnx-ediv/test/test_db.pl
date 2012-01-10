80a034a6a8f4923dbfd7a2fc405d61201097f701
david-fdez <david-fdez@localhost>
2012-01-10 01:22:04 +0100

    - more modifications to EDIV...not finished

--- vnx-ediv/test/test_db.pl
+++ vnx-ediv/test/test_db.pl
@@ -10,7 +10,7 @@ use Data::Dumper;
 
 $cluster_conf_file = "/etc/ediv/cluster.conf";
 
-print "--------------------------------------------------------------------\n";
+print "-----------------------------------------------\n";
 print "Testing database queries\n\n";
 print "Config file = $cluster_conf_file\n";
 
@@ -22,55 +22,64 @@ if (my $res = read_cluster_config) {
 
 my $query;
 my @response;
+my $error;
 
 $query = "INSERT INTO simulations VALUES ('example','7','22')";
-print "\nQuery = $query\n";
-query_db ($query);
+print "\n-------------------------------------------\nQuery = $query\n";
+$error = query_db ($query);
+if ($error) { die "** $error" }
 
 #$query = "SELECT `name`,`automac_offset`,`mgnet_offset` FROM simulations";
 $query = "SELECT * FROM simulations";
-print "\nQuery = $query\n";
-query_db ($query, \@response);
-print "\nResponse=\n" . Dumper(@response) . "\n";
+print "\n-------------------------------------------\nQuery = $query\n";
+$error = query_db ($query, \@response);
+if ($error) { die "** $error" }
 
+# Response comes in a two dimensional array 
+
+# print it with Dumper
+print "\n-------------------------------------------\nResponse:\n" 
+      . Dumper(@response) . "\n";
+
+# one way of walking through the array 
+print "\n-------------------------------------------\nResponse:\n"; 
+print "Number of rows=" . @response . "\n";
 foreach my $row (@response) {
-    foreach my $element (@$row) {
-        print $element, "\n";
+	print "Row:  ";
+    foreach my $field (@$row) {
+        if (defined($field)) { print $field . " "} else {print "undefined "; }
     }
+    print "\n";
 }
 
+# another way...
+print "\n-------------------------------------------\nResponse:\n";
 print "Number of rows=" . @response . "\n";
 for (my $i=0; $i < @response; $i++) {
-    print "Response $i ";
+    print "Row $i: ";
     my $row = $response[$i];
     #print "(@row):";
     for (my $j=0; $j < @$row; $j++) {
         #print "  $response[$i][$j],";
-        print "  $$row[$j],";
+        if (defined($$row[$j])) { print $$row[$j] . " "} else {print "undefined "; }
     }
     print "\n";
 }
 
 $query = "DELETE FROM simulations WHERE name = 'example'";
-print "\nQuery = $query\n";
-query_db ($query);
+print "\n-------------------------------------------\nQuery = $query\n";
+$error = query_db ($query);
+if ($error) { die "** $error" }
 
 undef @response;
 #$query = "SELECT `name` FROM simulations";
 $query = "SELECT * FROM simulations";
-print "\nQuery = $query\n";
-query_db ($query, \@response);
+print "\n-------------------------------------------\nQuery = $query\n";
+$error = query_db ($query, \@response);
+if ($error) { die "** $error" }
 
-print "Number of rows=" . $#response . "\n";
-for (my $i=0; $i <= $#response; $i++) {
-    print "Response $i: ";
-    my @row = $response[$i];
-    for (my $j=0; $j < @row; $j++) {
-        #print "  $response[$i][$j],";
-        print "  $row[$j],";
-    }
-    print "\n";
-}
+print "\n-------------------------------------------\nResponse:\n" 
+      . Dumper(@response) . "\n";
 
 print "\n";
 
@@ -78,43 +87,23 @@ sub query_db {
     
     my $query_string = shift;
     my $ref_response = shift;
+    my $error;
     
-    my $dbh = DBI->connect($db->{conn_info},$db->{user},$db->{pass}) or die "Cannot connect to database" . DBI->errstr;
-    my $query = $dbh->prepare($query_string);
-    $query->execute();
+    my $dbh = DBI->connect($db->{conn_info},$db->{user},$db->{pass}) 
+       or return "DB ERROR: Cannot connect to database. " . DBI->errstr;
+    my $query = $dbh->prepare($query_string) 
+       or return "DB ERROR: Cannot prepare query to database. " . DBI->errstr;
+    $query->execute()
+       or return "DB ERROR: Cannot execute query to database. " . DBI->errstr;
 
     if (defined($ref_response)) {
-        #my @response=@$ref_response;
-        #my $i=0;
         while (my @row = $query->fetchrow_array()) {
-=BEGIN
-            for (my $j=0; $j <= $#row; $j++) {
-                #print '$response[$i][$j] = $row[$j]' . "\n";
-                #print "value = $row[$j]\n"; 
-                $$ref_response[$i][$j] = $row[$j];
-                #print "value2[$i][$j] = $$ref_response[$i][$j]\n"; 
-              
-            }
-            $i++;
-=END
-=cut            
-
-
-        	#print "fields num = $#row\n";
-        	#foreach my $v (@row) { print "value = $v, "};
-            #print "\n";
-            #print "resp: @row\n";
-        	
             push (@$ref_response, \@row)
-                    	
         }
-        #print "Number of rows=" . $#response . "\n";
-        
-                
-        #@$ref_response = $query->fetchrow_array();
     }
-    #print "Number of rows=" . $#@(ref_response->) . "\n";   
     $query->finish();
     $dbh->disconnect;
+
+    return '';
 
 }
